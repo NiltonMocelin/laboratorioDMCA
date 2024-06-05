3407e8479744adff340289069bdb2363a84d42e6
NiltonMocelin <nilton.junior@edu.udesc.br>
2024-06-04 23:40:14 -0300

    material laboratorio

---
+++ processador_pacotes.py
@@ -0,0 +1,13 @@
+#classe que trata pacotes de fluxos e processa metricas uteis (list) para o classificador
+
+def processar_pacote(timestamp, _pacote) -> list:
+	
+	#extrair features do pacote --> ver quais metricas sao necessarias == mesmo que ja aconteceu la no ENIAC so copiar
+	
+	return []
+
+def processar_buffer(_buffer) -> list:
+	
+	#pegar cada feature de cada pacote e combinar para gerar uma entrada de classificacao --> mesmo que ja aconteceu la no ENIAC so copiar
+	
+	return []
